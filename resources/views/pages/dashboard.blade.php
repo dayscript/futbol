284c2ffbecb82e7a3f95718011358e5782857cca
Juan Carlos Orrego <jcorrego@gmail.com>
2015-10-13 15:20:20 -0500

    Dashboard styles

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -8,20 +8,19 @@
     <h2><i class="fa fa-bar-chart"></i> Dashboard</h2>
     <hr>
     <div class="row">
-        <div class="card col-md-3">
-            <div class="card-block">
+        <div class="col-md-3">
+            <div class="card card-block">
                 <h4 class="card-title">Opta Feeds</h4>
-                <p class="card-text">Total: {{ $optafeeds->count() }}</p>
+                <p class="card-text">Total de feeds recibidos a la fecha: <strong>{{ $optafeeds->count() }}</strong></p>
                 <a href="/optafeeds" class="btn btn-primary">Ver</a>
             </div>
         </div>
-        <div class="card col-md-3">
-            <div class="card-block">
+        <div class="col-md-3">
+            <div class="card card-block">
                 <h4 class="card-title">Usuarios</h4>
-                <p class="card-text">Total: {{ $users->count() }}</p>
+                <p class="card-text">Catntidad de usuarios creados en el sistema: <strong>{{ $users->count() }}</strong></p>
                 <a href="/users" class="btn btn-primary">Ver</a>
             </div>
         </div>
-
     </div>
 @stop
