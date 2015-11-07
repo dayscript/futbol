1712d7054b0efbbc95074895e781166f9cece3df
Juan Carlos Orrego <jcorrego@gmail.com>
2015-11-07 09:59:04 -0500

    Seria A fecha 12

--- resources/views/tournaments/widget.blade.php
+++ resources/views/tournaments/widget.blade.php
@@ -108,8 +108,8 @@
             <option value="7599">Todos contra todos/Fecha 8</option>
             <option value="7600">Todos contra todos/Fecha 9</option>
             <option value="7601">Todos contra todos/Fecha 10</option>
-            <option selected="" value="7602">Todos contra todos/Fecha 11</option>
-            <option value="7603">Todos contra todos/Fecha 12</option>
+            <option value="7602">Todos contra todos/Fecha 11</option>
+            <option selected="" value="7603">Todos contra todos/Fecha 12</option>
             <option value="7604">Todos contra todos/Fecha 13</option>
             <option value="7605">Todos contra todos/Fecha 14</option>
             <option value="7606">Todos contra todos/Fecha 15</option>
