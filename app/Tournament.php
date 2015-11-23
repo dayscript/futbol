44d553a2330c775ced23b3a40c0481c60b4cc364
Juan Carlos Orrego <jcorrego@gmail.com>
2015-11-23 08:08:51 -0500

    Various tournaments round change

--- app/Tournament.php
+++ app/Tournament.php
@@ -111,6 +111,13 @@ class Tournament extends Model
             Storage::disk('s3')->put('/resultswidget/150944_7604.txt',$content);
             Storage::disk('s3')->setVisibility('/resultswidget/150944_7604.txt', 'public');
         } else if($this->id == "150943"){ //Liga BBVA
+
+            $dates = ["2015-11-06","2015-11-07","2015-11-08"];
+            $view = View::make('tournaments.widget',compact('tournament','dates'));
+            $content = $view->render();
+            Storage::disk('s3')->put('/resultswidget/150943_7562.txt',$content);
+            Storage::disk('s3')->setVisibility('/resultswidget/150943_7562.txt', 'public');
+
             $dates = ["2015-11-21","2015-11-22","2015-11-23"];
             $view = View::make('tournaments.widget',compact('tournament','dates'));
             $content = $view->render();
