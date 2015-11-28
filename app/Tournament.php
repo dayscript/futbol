bf3c2fc16f653fe4aad4a7a855841ead5b510c20
Juan Carlos Orrego <jcorrego@gmail.com>
2015-11-28 11:15:54 -0500

    bbva 13

--- app/Tournament.php
+++ app/Tournament.php
@@ -120,13 +120,13 @@ class Tournament extends Model
 //            Storage::disk('s3')->put('/resultswidget/150943_7562.txt',$content);
 //            Storage::disk('s3')->setVisibility('/resultswidget/150943_7562.txt', 'public');
 
-            $dates = ["2015-11-21","2015-11-22","2015-11-23"];
+            $dates = ["2015-11-27","2015-11-28","2015-11-29"];
             $view = View::make('tournaments.widget',compact('tournament','dates'));
             $content = $view->render();
             Storage::disk('s3')->put('/resultswidget/150943_0.txt',$content);
             Storage::disk('s3')->setVisibility('/resultswidget/150943_0.txt', 'public');
-            Storage::disk('s3')->put('/resultswidget/150943_7563.txt',$content);
-            Storage::disk('s3')->setVisibility('/resultswidget/150943_7563.txt', 'public');
+            Storage::disk('s3')->put('/resultswidget/150943_7564.txt',$content);
+            Storage::disk('s3')->setVisibility('/resultswidget/150943_7564.txt', 'public');
 
         }
     }
