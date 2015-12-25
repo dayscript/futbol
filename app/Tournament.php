3be29989103722858ef8b4e8dd1bbac970de22f0
Juan Carlos Orrego <jcorrego@gmail.com>
2015-12-25 09:39:31 -0500

    Liga BBVA 17

--- app/Tournament.php
+++ app/Tournament.php
@@ -112,13 +112,14 @@ class Tournament extends Model
             Storage::disk('s3')->put('/resultswidget/150944_7608.txt',$content);
             Storage::disk('s3')->setVisibility('/resultswidget/150944_7608.txt', 'public');
         } else if($this->id == "150943"){ //Liga BBVA
-            $dates = ["2015-12-19","2015-12-20"];
-            $view = View::make('tournaments.widget',compact('tournament','dates'));
+            $dates = [];
+            $matchday = 17;
+            $view = View::make('tournaments.widget',compact('tournament','dates','matchday'));
             $content = $view->render();
             Storage::disk('s3')->put('/resultswidget/150943_0.txt',$content);
             Storage::disk('s3')->setVisibility('/resultswidget/150943_0.txt', 'public');
-            Storage::disk('s3')->put('/resultswidget/150943_7567.txt',$content);
-            Storage::disk('s3')->setVisibility('/resultswidget/150943_7567.txt', 'public');
+            Storage::disk('s3')->put('/resultswidget/150943_7568.txt',$content);
+            Storage::disk('s3')->setVisibility('/resultswidget/150943_7568.txt', 'public');
 
         }
     }
