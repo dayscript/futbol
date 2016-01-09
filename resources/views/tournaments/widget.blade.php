f021827459bf2ef64773a01c5c5fd95927f5a16b
Juan Carlos Orrego <jcorrego@gmail.com>
2016-01-09 13:43:16 -0500

    premier 21

--- resources/views/tournaments/widget.blade.php
+++ resources/views/tournaments/widget.blade.php
@@ -31,8 +31,8 @@
             <option value="7528">Todos contra todos/Fecha 17</option>
             <option value="7529">Todos contra todos/Fecha 18</option>
             <option value="7530">Todos contra todos/Fecha 19</option>
-            <option selected="" value="7531">Todos contra todos/Fecha 20</option>
-            <option value="7532">Todos contra todos/Fecha 21</option>
+            <option value="7531">Todos contra todos/Fecha 20</option>
+            <option selected="" value="7532">Todos contra todos/Fecha 21</option>
             <option value="7533">Todos contra todos/Fecha 22</option>
             <option value="7534">Todos contra todos/Fecha 23</option>
             <option value="7535">Todos contra todos/Fecha 24</option>
